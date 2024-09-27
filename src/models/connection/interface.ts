6d58c9d3dd360d728e9cfa90cd1065357072b71d
Joao Cardoso <joao.cardoso@nn.nl>
2024-09-27 11:57:18 +0200

    linting

--- src/models/connection/interface.ts
+++ src/models/connection/interface.ts
@@ -1,13 +1,13 @@
 export interface ICredential {
-    refresh_token?: string
-    access_token?: string
-  }
-  
-  export interface IAppConnection {
-    appId: string
-    userId: string
-    connectionId: string
-    email: string
-    name: string
-    credentials: ICredential
-  }
+  refresh_token?: string
+  access_token?: string
+}
+
+export interface IAppConnection {
+  appId: string
+  userId: string
+  connectionId: string
+  email: string
+  name: string
+  credentials: ICredential
+}
